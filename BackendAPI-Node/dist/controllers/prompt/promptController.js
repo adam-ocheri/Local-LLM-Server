a01710ba60de43f1adc87d9a9891df9e7790a6fc
Adam Ocheri <adamotchery@gmail.com>
2023-08-24 05:31:45 +0300

    setting up flask asyncio

--- BackendAPI-Node/dist/controllers/prompt/promptController.js
+++ BackendAPI-Node/dist/controllers/prompt/promptController.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseAPI_URL = `http://localhost:5000/`;
 export async function generateBasicPrompt(req, res) {
     try {
-        const { modelName } = req.params;
+        const { modelName } = req.query;
         const response = await axios.post(baseAPI_URL + `?model=${modelName}`, req.body);
         res.json(response.data);
         // res.json(`Hello Response! | ${modelName} | ${prompt}`)
--- BackendAPI-Node/src/controllers/prompt/promptController.ts
+++ BackendAPI-Node/src/controllers/prompt/promptController.ts
@@ -3,7 +3,7 @@ const baseAPI_URL = `http://localhost:5000/`;
 
 export async function generateBasicPrompt(req : any, res : any)  {
     try {
-        const {modelName} = req.params;
+        const {modelName} = req.query;
         const response = await axios.post(baseAPI_URL + `?model=${modelName}`, req.body);
         res.json(response.data);
         // res.json(`Hello Response! | ${modelName} | ${prompt}`)
