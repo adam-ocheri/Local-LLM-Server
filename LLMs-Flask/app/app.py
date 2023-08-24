117ba11617b86380ee137c5d644b32fbac0b9773
Adam Ocheri <adamotchery@gmail.com>
2023-08-24 17:57:21 +0300

    fully implemented Quart async ops

--- LLMs-Flask/app/app.py
+++ LLMs-Flask/app/app.py
@@ -21,14 +21,14 @@ cors(app)
 
 global active_model
 active_model_name = "meta-llama/Llama-2-7b-hf"
-# active_model = ModelHF(active_model_name, "/LLMs-Flask/models/" + active_model_name)
+active_model = ModelHF(active_model_name, "/LLMs-Flask/models/" + active_model_name)
 
 
-# async def on_model_set(name=""):
-#     active_model_name = name
-#     active_model = await ModelHF(
-#         active_model_name, "/LLMs-Flask/models/" + active_model_name
-#     )
+async def on_model_set(name=""):
+    active_model_name = name
+    active_model = await ModelHF(
+        active_model_name, "/LLMs-Flask/models/" + active_model_name
+    )
 
 
 #! ROUTES - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -39,21 +39,21 @@ async def test_front():
     return "<h1>Python LlamaV2 Hello!<h1>"
 
 
-# def prompt_extraction_middleware(view_func):
+# async def prompt_extraction_middleware(view_func):
 #     @wraps(view_func)
-#     def wrapper(*args, **kwargs):
+#     async def wrapper(*args, **kwargs):
 #         print(
 #             "REQUESTING ----------------------------------------------------------------------"
 #         )
 #         mimetype = request.mimetype
 #         if mimetype == "application/x-www-form-urlencoded":
-#             data = json.loads(next(iter(request.form.keys())))
+#             data = await request.form
 #         elif mimetype == "multipart/form-data":
-#             data = dict(request.form)
+#             data = await dict(request.form)
 #         elif mimetype == "application/json":
-#             data = request.json
+#             data = await request.json
 #         else:
-#             data = request.data.decode()
+#             data = (await request.data).decode()
 
 #         print(mimetype, data, type(data))
 
@@ -64,37 +64,31 @@ async def test_front():
 #     return wrapper
 
 
-# def select_active_model(view_func):
+# async def select_active_model(view_func):
 #     @wraps(view_func)
-#     def wrapper(*args, **kwargs):
+#     async def wrapper(*args, **kwargs):
 #         requested_model_name = request.args.get("model")
 
 #         if requested_model_name != active_model_name:
-#             on_model_set(requested_model_name)
-#             #! Find a way to Async/Await here!
+#             await on_model_set(requested_model_name)
 
 #         return view_func(*args, **kwargs)
 
 #     return wrapper
 
 
-# @app.route("/gen", methods=["POST"])
-# @prompt_extraction_middleware
-# @select_active_model
-# def generate(prompt):
-#     # prompt = request.json.get("prompt")
-#     input_text = prompt
-#     inputs = active_model.tokenizer.encode(input_text, return_tensors="pt")
-#     outputs = active_model.model.generate(
-#         inputs, max_length=50, num_return_sequences=1, temperature=0.7
-#     )
+@app.route("/gen", methods=["POST"])
+async def generate():
+    input_data = await request.get_json()
+    prompt = input_data.get("prompt")
+    input_text = prompt
+    inputs = active_model.tokenizer.encode(input_text, return_tensors="pt")
+    outputs = active_model.model.generate(
+        inputs, max_length=50, num_return_sequences=1, temperature=0.7
+    )
+    response_text = active_model.tokenizer.decode(outputs[0])
+    return jsonify({"response": response_text}), 200
 
-#     print("response is:", active_model.tokenizer.decode(outputs[0]))
-
-#     # loop = asyncio.get_event_loop()
-#     # loop.run_until_complete(generate())
-
-#     return jsonify({"response": active_model.tokenizer.decode(outputs[0])}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
