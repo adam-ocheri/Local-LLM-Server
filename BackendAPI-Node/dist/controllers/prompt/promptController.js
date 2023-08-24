25643564322bb0a343a6f36ec438d1f1a540cd2f
Adam Ocheri <adamotchery@gmail.com>
2023-08-24 05:22:08 +0300

    added axios post request

--- BackendAPI-Node/dist/controllers/prompt/promptController.js
+++ BackendAPI-Node/dist/controllers/prompt/promptController.js
@@ -1,8 +1,11 @@
+import axios from "axios";
+const baseAPI_URL = `http://localhost:5000/`;
 export async function generateBasicPrompt(req, res) {
     try {
         const { modelName } = req.params;
-        const { prompt } = req.body;
-        res.json(`Hello Response! | ${modelName} | ${prompt}`);
+        const response = await axios.post(baseAPI_URL + `?model=${modelName}`, req.body);
+        res.json(response.data);
+        // res.json(`Hello Response! | ${modelName} | ${prompt}`)
     }
     catch (error) {
         throw new Error(error);
--- BackendAPI-Node/src/controllers/prompt/promptController.ts
+++ BackendAPI-Node/src/controllers/prompt/promptController.ts
@@ -1,9 +1,12 @@
+import axios from "axios";
+const baseAPI_URL = `http://localhost:5000/`;
 
 export async function generateBasicPrompt(req : any, res : any)  {
     try {
         const {modelName} = req.params;
-        const {prompt} = req.body;
-        res.json(`Hello Response! | ${modelName} | ${prompt}`)
+        const response = await axios.post(baseAPI_URL + `?model=${modelName}`, req.body);
+        res.json(response.data);
+        // res.json(`Hello Response! | ${modelName} | ${prompt}`)
     } catch (error : any) {
         throw new Error(error)
     }
